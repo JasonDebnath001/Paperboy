8e98cc6e8468a1177f9ed603d1b4d92d5a5ebe1c
Aritra Debnath <aritradebnath1@gmail.com>
2025-12-13 20:54:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const PDFDocument = require("pdfkit");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
